e6c80bdbb931a24cea670b283cd8150d1e79cf9a
jocelin queau <queau.j@gmail.com>
2022-03-08 20:03:04 +0100

    :tada:

---
+++ main.c
@@ -0,0 +1,41 @@
+
+
+
+int main(int argc, char* argv[]){
+    // 1. get les setting de parties ( nb colonnes, lignes) 
+    // 2. savoir qui commence
+    // 3. initailiser le board
+    //* 
+    // 4.TOUR 
+    // jeu va serparer en tour
+        // j1 joue
+        // check de si gagne ou pas
+        // *
+        // j2 joue
+        // check de si gagne ou pas
+        // *
+    // *6. print le tableau representatif de la partie
+    // 7. fin de partie free
+    
+}
+
+// 1 - flag maybe
+// argv desctrurer
+// regles de flag?  et nb col 
+// derouler avec scanf etc les joueurs
+// qui commence   menu 1-J 2-A
+ 
+// ** ou [][] de taille ptr de ptr  valeur x | o - malloc
+
+
+// logique de tour
+// print tab
+// J-1 joue choisi colonne | 1 , 2 , 3 etc si pas valide reprint
+// check win - check pas complete
+// j-2
+// check win - check pas complete
+
+// while not flag var true -> play
+// print winner
+// 7. free
+
